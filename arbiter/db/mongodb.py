81d0c0fd2d084cff9ec3b07396a5f48aa89efb30
yang123456789 <1877084583@qq.com>
2018-08-06 22:15:45 +0800

    数据表

--- arbiter/db/mongodb.py
+++ arbiter/db/mongodb.py
@@ -1,6 +1,48 @@
 from mongoengine import *
+import pytz
+from datetime import datetime
+
+_datetime = datetime.now(tz=pytz.timezone('Asia/Shanghai'))
 
 # mongodb
 connect('reptile', host='123.58.244.170', port=27017, username='yangjiajia', password='SYHXsqq@1233')
 
 
+class Recruit(Document):
+    create_at = DateTimeField(default=_datetime)
+    # 职位的名称
+    name = StringField(max_length=80, null=False)
+    # 薪水
+    salary = StringField(max_length=12, null=False)
+    # 工作地点
+    address = StringField(max_length=100, null=False)
+    # 学历
+    education = StringField(max_length=10, null=False)
+    # 工作年限
+    work_time = StringField(max_length=12)
+    # 福利待遇
+    welfare = ListField()
+    # 是否全职
+    emplType = StringField(max_length=10)
+    # 公司的名称
+    company = StringField(max_length=100)
+    # 公司性质
+    company_nature = StringField(max_length=20)
+    # 公司规模
+    company_size = StringField(max_length=20)
+    # 岗位职责
+    responsibility = ListField()
+    # 任职要求
+    tenure = ListField()
+    # 工作职责
+    work_responsibility = ListField()
+
+    def __str__(self):
+        return self.name
+
+    meta = {
+        'indexes': [
+            'create_at', 'name'
+        ],
+        'ordering': ['-create_at']
+    }
--- arbiter/portal/recruit.py
+++ arbiter/portal/recruit.py
@@ -1,8 +1,13 @@
 import requests
 import re
 import logging
+import ujson
 
-logger = logging.getLogger('sourceDns.webdns.views')
+logger = logging.getLogger('''sourceDns.webdns.views''')
+header = {
+    '''User-Agent''': '''Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 '''
+                  '''(KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'''
+}
 
 
 class Base:
@@ -16,7 +21,7 @@ class Data(Base):
 
     def request(self):
         try:
-            req = requests.get(self.url, timeout=5)
+            req = requests.get(self.url, timeout=5, headers=header)
             content = req.text
             req.close()
             return content
@@ -28,33 +33,46 @@ class Data(Base):
 class ZhiLian(Data):
     def handle(self):
         content = self.request()
-        start = content.find('<table width="853" class="newlist" cellpadding="0" cellspacing="0">')
-        end = content.rfind('</table>')
-        info = content[start: end+8]
-        parttern = re.compile(r'<[^>]+>', re.S)
-        data = parttern.sub('', info)
-        msg = data.replace(' ', '')
-        ret = msg.split('\r\n')
-        result = list()
-        for r in ret[1:]:
-            if r != '':
-                result.append(r)
-        return result
+        try:
+            msg = ujson.loads(content)
+        except Exception as e:
+            logger.error(e)
+            msg = {}
+        if msg and msg.get('code'):
+            return msg['data']['results']
+
+    def get_data(self, content):
+        for con in content:
+            pass
+
+    def get_details(self, url):
+        try:
+            req = requests.get(url, headers=header)
+            if req.status_code is 200:
+                return req.text
+        except Exception as e:
+            logger.error(e)
+            return
+# url = '''https://fe-api.zhaopin.com/c/i/sou?start=60&pageSize=60&cityId=530&industry=10100&workExperience=-1&education=-1&companyType=-1&employmentType=-1&jobWelfareTag=-1&kw=Python&kt=3&lastUrlQuery=%7B%22jl%22:%22530%22,%22in%22:%2210100%22,%22kw%22:%22Python%22,%22kt%22:%223%22%7D'''
+# a = ZhiLian(url)
+# print(a.handle())
 
 
 class LiePin(Data):
     def handle(self):
         content = self.request()
-        start = content.find('<ul class="sojob-list">')
-        end = content.rfind('<div class="pager">')
+        start = content.find('''<ul class="sojob-list">''')
+        end = content.rfind('''<div class="pager">''')
         info = content[start: end]
-        parttern = re.compile(r'<[^>]+>', re.S)
-        data = parttern.sub('', info)
+        parttern = re.compile(r'''<[^>]+>''', re.S)
+        data = parttern.sub('''''', info)
 
-        msg = data.replace(' ', '')
-        message = re.compile(r'/\t+', re.S).sub('', msg)
-        ret = message.split('\r\n')
-        result = [r.replace('\t', '') for r in ret if '\t' in r and r]
-        while '' in result:
-            result.remove('')
+        msg = data.replace(''' ''', '''''')
+        message = re.compile(r'''/\t+''', re.S).sub('''''', msg)
+        ret = message.split('''\r\n''')
+        result = [r.replace('''\t''', '''''') for r in ret if '''\t''' in r and r]
+        while '''''' in result:
+            result.remove('''''')
         return result
+
+b = '''https://fe-api.zhaopin.com/c/i/sou/page-title?start=60&pageSize=60&cityId=530&areaId=&businessarea=%7B%7D&industry=10100&salary=&workExperience=-1&education=-1&companyType=-1&employmentType=-1&jobWelfareTag=-1&jobType=-1&sortType=&kw=Python&kt=3&bj=&sj=&lastUrlQuery=%7B%22p%22:2,%22jl%22:%22530%22,%22in%22:%2210100%22,%22kw%22:%22Python%22,%22kt%22:%223%22%7D'''
